27fd25afecff7d7b029da2607d573a2a94931289
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 20:31:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,5 +81,15 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10()
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT number
+      FROM students
+      WHERE grade = 10
+    SQL
+    
+    DB[:conn].execute(sql,num).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end

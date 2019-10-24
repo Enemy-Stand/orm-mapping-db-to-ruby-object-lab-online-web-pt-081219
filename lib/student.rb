70477b46a7809bcf41d6f67b4bfdc02b861c3b59
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 20:18:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,10 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT name
+      SELECT *
       FROM students
+      WHERE name = ?
+      LIMIT 1
     SQL
     
     DB [:conn].execute(sql).map.do |row|

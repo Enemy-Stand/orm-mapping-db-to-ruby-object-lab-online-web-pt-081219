eb59fe89f4269ca012f8a4ecfb6e634dc967a9a7
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 20:35:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,11 +86,25 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      ORDER BY studentS.id
+      ORDER BY students.id
       LIMIT ?
     SQL
     
     DB[:conn].execute(sql,num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
